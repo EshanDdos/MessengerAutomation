34440cf7a905aefe57f480a3ecd48c46ae60555a
Nusrat Doula Eshan <65530543+EshanDdos@users.noreply.github.com>
2021-06-07 14:04:12 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyautogui
+import time
+while True:
+    time.sleep(3)Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+    Hi, This side David form Bangladesh. This message is automated
+
+    pyautogui.typewrite('Hi, This side David form Bangladesh. This message is automated')
+    pyautogui.press('enter')
